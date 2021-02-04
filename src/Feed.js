bd1e8e43a3527d38ea81c5ffec64bfdf4e9f7b3f
JSolis <jerez.solis@gmail.com>
2021-02-04 13:49:16 -0800

    updated files

--- src/Feed.js
+++ src/Feed.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Feed.css';
 import StoryReel from './StoryReel';
 import MessageSender from './MessageSender';
+import Post from './Post';
 
 function Feed() {
   return (
@@ -9,8 +10,19 @@ function Feed() {
       <StoryReel />
       <MessageSender />
 
-      <Post />
-      <Post />
+      <Post
+        profilePic="https://www.usnews.com/dims4/USNEWS/cbc9554/2147483647/resize/1200x%3E/quality/85/?url=https%3A%2F%2Fmedia.beam.usnews.com%2F39%2F19e2a5e97bbabb3dd90fee9115c764%2Fmedia%3A8b59cdedb48843219428d703b41a53b0UFC_246_Mixed_Martial_Arts_80657.jpg"
+        message="Wow this works!"
+        timestamp="timestamp"
+        username="Conor Mcgregor"
+        image="https://www.thesun.ie/wp-content/uploads/sites/3/2020/09/as-graphi-tale-of-tape-mcgreggor-pacquiao-v2.jpg?strip=all&quality=100&w=1200&h=800&crop=1"
+      />
+      <Post
+        profilePic="https://www.usnews.com/dims4/USNEWS/cbc9554/2147483647/resize/1200x%3E/quality/85/?url=https%3A%2F%2Fmedia.beam.usnews.com%2F39%2F19e2a5e97bbabb3dd90fee9115c764%2Fmedia%3A8b59cdedb48843219428d703b41a53b0UFC_246_Mixed_Martial_Arts_80657.jpg"
+        message="Wow this works!"
+        timestamp="timestamp"
+        username="Conor Mcgregor"
+      />      
       <Post />
     </div>
   );
--- src/Post.js
+++ src/Post.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './Post.css';
 import { Avatar } from "@material-ui/core";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import { ExpandMoreOutlined } from "@material-ui/icons/NearMe";
 
 
 function Post({ profilePic, image, username, timestamp, message }) {
@@ -13,11 +17,24 @@ function Post({ profilePic, image, username, timestamp, message }) {
           <p>Timestamp...</p>
           {/* <p>{new Date(timestamp?.toDate()).toUTCString()}</p> */}
         </div>
-
-
-
       </div>
 
+
+      <div className="post__bottom">
+        <p>{message}</p>         
+      </div>
+
+
+      <div className="post__image">
+        <img src={image} alt="image"/>
+      </div>
+
+      <div className="post__options">
+        <div className="post__option">
+          <ThumbUpIcon />
+          <p>Like</p>
+        </div>
+      </div>
 
     </div>
   );
