151d66363e12cd49593df9a6ce10ba5c419afc7c
JSolis <jerez.solis@gmail.com>
2021-02-03 23:33:38 -0800

    updated files

--- src/Header.js
+++ src/Header.js
@@ -5,13 +5,20 @@ import SearchIcon from '@material-ui/icons/Search';
 import HomeIcon from '@material-ui/icons/Home';
 import FlagIcon from '@material-ui/icons/Flag';
 import SubscriptionsOutlinedIcon  from '@material-ui/icons/SubscriptionsOutlined';
-import StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlinedgit';
+import StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlined';
 import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import { Avatar, IconButton } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import ForumIcon from '@material-ui/icons/Forum';
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 
 //Header is seperated into 3 sections, left, middle and right
 function Header() {
   return (
     <div className="header">
+      {/* header left section */}
       <div className="header__left">
         <img src={FacebookLogo} alt="facebook logo"/>
         <div className="header__input">
@@ -20,26 +27,54 @@ function Header() {
         </div> 
       </div>
 
-      <div className="header__middle">
+      {/* header middle ection */}
+      <div className="header__center">
         <div className="header__option">
           <HomeIcon fontSize="large"/>
         </div>
+
         <div className="header__option">
           <FlagIcon fontSize="large"/>
         </div>  
+
         <div className="header__option">
           <SubscriptionsOutlinedIcon fontSize="large"/>
         </div>   
+
         <div className="header__option">
           <StorefrontOutlinedIcon fontSize="large"/>
         </div>   
+
         <div className="header__option">
           <SupervisedUserCircleIcon fontSize="large"/>
         </div>                                 
       </div>
       
 
-      <div className="header__right"></div>
+      <div className="header__right">
+        <div className="header__info">
+          <Avatar />
+          <h4>Jerez</h4>
+        </div>
+
+        <IconButton>
+          <AddIcon />
+        </IconButton>
+
+        <IconButton>
+          <ForumIcon />
+        </IconButton>
+
+        <IconButton>
+          <NotificationsActiveIcon />
+        </IconButton>
+
+        <IconButton>
+          <ExpandMoreIcon />
+        </IconButton>                
+
+
+      </div>
     </div>
   );
 }
